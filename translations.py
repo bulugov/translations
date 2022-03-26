2d507e7fac9d62ca75f27f877ff9fe9abd97f678
Bakhodir Ulugov <101068416+bulugov@users.noreply.github.com>
2022-03-27 00:52:19 +0400

    Update translations.py

--- translations.py
+++ translations.py
@@ -1,13 +1,19 @@
 import json
 
+
+#Open the required file
 f = open('/Users/bakhodirulugov/Desktop/webserver/countries.json')
-   
+ 
+#Assign a variable with data from .json file
 data = json.load(f)
 
+#Define the accepted keys for the function
 keys=['cym','deu','fra','hrv','ita','jpn','nld','por','rus','spa']
 
+#Ask the user for an input and assign it to the variable
 key = input('Enter the key:')
 
+#Translation function that takes user input as a parameter and checks if it fits the requirements in if-else statement
 def translate(key):
     if key not in keys:
         print('Key is not supported!')
